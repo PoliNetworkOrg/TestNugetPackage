85d06acb9e97270e015c4cc6399c1bd9bdd54f86
github.nouser@nouser.github.com <github.nouser@nouser.github.com>
2023-05-20 13:34:23 +0200

    Update SerializeUtil.cs

--- SampleNuGet/Utils/SerializeUtil.cs
+++ SampleNuGet/Utils/SerializeUtil.cs
@@ -1,6 +1,43 @@
-﻿namespace SampleNuGet.Utils;
+﻿using System.Text;
+using Newtonsoft.Json;
 
-public class SerializeUtil
+namespace SampleNuGet.Utils;
+
+//System.Runtime.Serialization.Formatters.Binary.BinaryFormatter is obsolete, replaced with
+//https://stackoverflow.com/a/53078979
+
+
+/// <summary>
+/// Serialization util
+/// </summary>
+public static class SerializeUtil
 {
-    
+    /// <summary>
+    /// Serialize object
+    /// </summary>
+    /// <param name="value">object to serialize</param>
+    /// <returns>byte array of serialized object</returns>
+    public static byte[] SerializeObject(object? value) => Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(value));
+
+    /// <summary>
+    /// Deserialize object, given the type (T)
+    /// </summary>
+    /// <param name="bytes">byte array of the serialized object to deserialize</param>
+    /// <typeparam name="T">type of the object</typeparam>
+    /// <returns>the deserialized object</returns>
+    public static T? DeserializeObject<T>(byte[] bytes)
+    {
+        var x = Encoding.UTF8.GetString(bytes);
+        return JsonConvert.DeserializeObject<T>(x);
+    }
+
+    /// <summary>
+    /// Get memory stream from byte array
+    /// </summary>
+    /// <param name="bytes">byte array</param>
+    /// <returns>memory stream</returns>
+    public static MemoryStream GetMemoryStreamFromByteArray(byte[] bytes)
+    {
+        return new MemoryStream(bytes);
+    }
 }
