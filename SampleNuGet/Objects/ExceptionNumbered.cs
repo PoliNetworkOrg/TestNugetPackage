5156597c46ec177255ecbb6820f39921a409423f
angeousta <angeousta@outlook.it>
2023-06-25 13:13:53 +0200

    Update ExceptionNumbered.cs, EventArgsContainer.cs, TelegramBotAbstract.cs, and 18 more files

--- SampleNuGet/Objects/ExceptionNumbered.cs
+++ SampleNuGet/Objects/ExceptionNumbered.cs
@@ -1,8 +1,6 @@
 ﻿#region
 
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using SampleNuGet.Objects.Exceptions;
 
 #endregion
 
@@ -37,101 +35,6 @@ public class ExceptionNumbered : Exception
     public int GetNumberOfTimes()
     {
         return _v;
-    }
-
-    internal static async Task<bool> SendExceptionAsync(Exception? e,
-        SampleNuGet.Objects.TelegramBotAbstract? telegramBotAbstract,
-        EventArgsContainer? eventArgsContainer)
-    {
-        if (telegramBotAbstract == null)
-            return false;
-
-        await NotifyUtil.NotifyOwnerWithLog2(e, telegramBotAbstract, eventArgsContainer);
-        return true;
-    }
-
-    public TelegramFileContent GetMessageAsText(
-        ExtraInfo? extraInfo,
-        EventArgsContainer? messageEventArgs,
-        bool json
-    )
-    {
-        if (json)
-        {
-            var jObject = new JObject
-            {
-                ["number"] = GetNumberOfTimes(),
-                ["message"] = Message,
-                ["ExceptionToString"] = ToString(),
-                ["StackTrace"] = GetStackTrace(StackTrace),
-                ["MessageArgs"] = messageEventArgs == null ? null : JsonConvert.SerializeObject(messageEventArgs),
-                ["extraInfo"] = extraInfo == null ? null : JsonConvert.SerializeObject(extraInfo)
-            };
-            var s2 = new StringJson(FileTypeJsonEnum.OBJECT, jObject);
-            return new TelegramFileContent(s2, null);
-        }
-
-        string message3;
-        try
-        {
-            message3 = "";
-            try
-            {
-                message3 += "Number of times: ";
-                message3 += GetNumberOfTimes();
-                message3 += "\n\n";
-            }
-            catch
-            {
-                message3 += "\n\n";
-            }
-
-            try
-            {
-                message3 += "Message:\n";
-                message3 += Message;
-                message3 += "\n\n";
-            }
-            catch
-            {
-                message3 += "\n\n";
-            }
-
-            try
-            {
-                message3 += "ExceptionToString:\n";
-                message3 += ToString();
-                message3 += "\n\n";
-            }
-            catch
-            {
-                message3 += "\n\n";
-            }
-
-
-            if (messageEventArgs != null)
-                try
-                {
-                    message3 += "MessageArgs:\n";
-                    message3 += JsonConvert.SerializeObject(messageEventArgs);
-                }
-                catch
-                {
-                    message3 += "\n\n";
-                }
-
-            var genericInfo = extraInfo?.GenericInfo;
-            if (!string.IsNullOrEmpty(genericInfo))
-                message3 += "\n\n" + genericInfo;
-        }
-        catch (Exception e1)
-        {
-            message3 = "Error in sending exception: this exception occurred:\n\n" + e1.Message;
-        }
-
-        var serializeObject = "StackTrace:\n" + JsonConvert.SerializeObject(GetStackTrace(StackTrace));
-        var serializeObject2 = new StringJson(FileTypeJsonEnum.SIMPLE_STRING, serializeObject);
-        return new TelegramFileContent(serializeObject2, message3);
     }
 
     private static JObject GetStackTrace(string? stackTracePar)
