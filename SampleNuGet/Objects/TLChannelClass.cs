5d096160e375aa720ce41bbce21e6b5b6fd83752
angeousta <angeousta@outlook.it>
2023-06-25 22:24:19 +0200

    Update CallbackQueryEventArgs.cs, ExceptionNumbered.cs, Language.cs, and 5 more files

--- SampleNuGet/Objects/TLChannelClass.cs
+++ SampleNuGet/Objects/TLChannelClass.cs
@@ -1,17 +1,1 @@
-﻿#region
-
-using TeleSharp.TL;
-
-#endregion
-
-namespace SampleNuGet.Objects;
-
-public class TlChannelClass
-{
-    public readonly TLChannel Channel;
-
-    public TlChannelClass(TLChannel channel)
-    {
-        Channel = channel;
-    }
-}
+﻿
